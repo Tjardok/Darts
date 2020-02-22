0d8f1ef13e9c10b3b5433fc307066787915d32fa
TKnopper <53013529+TKnopper@users.noreply.github.com>
2020-02-22 13:49:10 +0100

    Update darts.js

--- darts.js
+++ darts.js
@@ -1,32 +1,17 @@
 //Event for get table cell Id
-//document.getElementById("table").onclick = getCells;
-
-
-
-
-    // document.getElementById("table").onclick = getCells;
     var tbl = document.getElementById("table");
-     for (var i = 0; i < tbl.rows.length; i++) {
-         for (var j = 0; j < tbl.rows[i].cells.length; j++)
- 
-             tbl.rows[i].cells[j].onclick = function () {
-                 getval(this);
- 
-             };
- 
-     }
+    for (var i = 0; i < tbl.rows.length; i++) {
+        for (var j = 0; j < tbl.rows[i].cells.length; j++)
 
+            tbl.rows[i].cells[j].onclick = function () {
+                getval(this);
 
+            };
 
-  
-
-
-
-//}
+    }
 
 function getval(cel) {
 
-    
     switch (cel.innerHTML) {
 
         case '':
@@ -47,6 +32,7 @@ function getval(cel) {
 
         case '111':
             getNeighbours(cel);
+
             break;
 
     }
@@ -88,57 +74,51 @@ function getNeighbours(cell) {
 var checked = document.getElementById("myCheck").checked;
 document.getElementById("myCheck").onchange = getCheck;
 console.log(checked);
- //checked == false 
+//checked == false 
 
 function getCheck() {
     console.log('getCheck');
-   var check = document.getElementById("myCheck").checked;
-   // console.log(check);
-   if (check == false) {
+    var check = document.getElementById("myCheck").checked;
+    // console.log(check);
+    if (check == false) {
         var tbl = document.getElementById("table");
         for (var i = 0; i < tbl.rows.length; i++) {
             for (var j = 0; j < tbl.rows[i].cells.length; j++)
                 tbl.rows[i].cells[j].onclick = function () {
                     Minus(this)
 
-                }
+             }
 
-          }
+        }
 
+    }
 
-   }
-
+    else if (check == true) {
+        getCells();
+    }
 }
 
 
 function Minus(minCel) {
-   // console.log(check);
-   // var check = document.getElementById("myCheck").checked;
-
     switch (minCel.innerHTML) {
 
         case '1':
             minCel.innerHTML = "";
             minCel.style.backgroundColor = 'unset';
             break;
-           
 
         case '11':
             minCel.innerHTML = 1;
-            minCel.style.backgroundColor = 'red';   
+            minCel.style.backgroundColor = 'red';
             break;
 
         case '111':
             minCel.innerHTML = 11;
             minCel.style.backgroundColor = 'orange';
             break;
-    }
-
-
+      }
 
 }
-
-
 
 //-------------------------------Functions to CLEAR score ---------------------------------------------//
 document.getElementById("clear").onclick = clearScore;
@@ -155,8 +135,6 @@ function clearScore() {
                 x.innerHTML = "";
                 x.style.backgroundColor = 'unset';
 
-            }
-
-
+           }
     }
 }
