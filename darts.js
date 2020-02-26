a4e9addf8d76b1714e503426e381a6cd699faa4b
Tjardok <>
2020-02-26 18:53:13 +0100

    functie  show image

--- darts.js
+++ darts.js
@@ -4,15 +4,16 @@ document.getElementById("table").onclick = getTable();
 function getTable() {
 
     var tbl = document.getElementById("table");
-    console.log('start');
+
     for (var i = 0; i < tbl.rows.length; i++) {
         for (var j = 0; j < tbl.rows[i].cells.length; j++)
             tbl.rows[i].cells[j].onclick = function () {
                 getval(this);
-               
+                getCount();
+
 
             }
-     }
+    }
 }
 
 
@@ -42,7 +43,6 @@ function getval(cel) {
 
             case '111':
                 getNeighbours(cel);
-                console.log(this);
                 break;
         }
 
@@ -123,7 +123,7 @@ function clearScore() {
 
 
 
-//-------------------------------Functios to add and remove Player---------------------------------------------------//
+//-------------------------------Functions to add and remove Player---------------------------------------------------//
 
 document.getElementById("remove").onclick = removePlayer;
 
@@ -157,16 +157,13 @@ document.getElementById("add").onclick = addPlayer;
 
 function addPlayer() {
     [...document.querySelectorAll('#table tr')].forEach((row, i) => {
-      
 
-       let cell = document.createElement(i ? "td" : "th")
+
+        let cell = document.createElement(i ? "td" : "th")
         cell.setAttribute('contenteditable', 'true')
-       row.appendChild(cell)
-     
-       cell.onclick = getTable(cell);
+        row.appendChild(cell)
 
-
-
+        cell.onclick = getTable(cell);
 
 
     });
@@ -174,3 +171,111 @@ function addPlayer() {
 
 
 //-------------------------------Functions hide and show image ----------------------------------------------//
+
+//Hide image default
+document.getElementById("myImg").style.display = "none";
+
+function openFullImage() {
+    document.getElementById("myImg").style.display = "block";
+    var elem = document.getElementById("myImg");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+
+
+
+}
+
+
+//-------------------------- Fullscreen mode function -----------------//
+/* Get the element you want displayed in fullscreen mode (a video in this example): */
+document.getElementById("fullscreen").onclick = openFullscreen;
+
+/* When the openFullscreen() function is executed, open the table in fullscreen. 
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+
+    var elem = document.getElementById("table");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+
+document.getElementById("NUM").onclick = closeFullscreen;
+document.getElementById("myImg").onclick = closeFullscreen;
+
+/* Close fullscreen */
+function closeFullscreen() {
+    document.getElementById("myImg").style.display = "none";
+
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
+
+
+
+//-------------------------- Table value count function -----------------------//
+
+function getCount() {
+
+    var table = document.getElementById('table'),
+    sumVal1 = 0;
+    sumVal2 = 0;
+    sumVal3 = 0;
+    sumVal4 = 0;
+    for (var i = 1; i < table.rows.length; i++) {
+        sumVal1 = sumVal1 + table.rows[i].cells[1].innerHTML;
+        sumVal2 = sumVal2 + table.rows[i].cells[2].innerHTML;
+        sumVal3 = sumVal3 + table.rows[i].cells[3].innerHTML;
+        sumVal4 = sumVal4 + table.rows[i].cells[4].innerHTML;
+
+    }
+    if (sumVal1 == '0111111111111111111111111111111111111') {
+        openFullImage();
+    }
+
+  
+    if (sumVal2 == '0111111111111111111111111111111111111') {
+        openFullImage();
+
+    }
+    if (sumVal3 == '0111111111111111111111111111111111111') {
+        openFullImage();
+
+    }
+
+    if (sumVal4 == '0111111111111111111111111111111111111') {
+        openFullImage();
+
+    }
+
+}
